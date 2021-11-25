3b8c79bf744811ea2b5beae8f403174c3e0f4e63
Leo7Maia <maiahayabusa.leo@gmail.com>
2021-11-25 10:01:15 -0300

    ..

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  HOST: "database-1-seusdados-instance-1.cvnip89kz3fi.sa-east-1.rds.amazonaws.com",
+  HOST: "database-1-seusdados.cluster-cvnip89kz3fi.sa-east-1.rds.amazonaws.com",
   USER: "shipping_cali_app",
   PASSWORD: "zbi2b4HYz8]eoAy3,@*c",
   DB: "db_shipping_cali"
