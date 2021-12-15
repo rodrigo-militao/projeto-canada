d5648b0eee3f39c3b8798dce16a326e96b687e87
rodrigo-militao <militaodorigo9@gmail.com>
2021-12-15 10:17:32 -0300

    dashboard changes

--- app/controllers/dashboard.controller.js
+++ app/controllers/dashboard.controller.js
@@ -3,24 +3,37 @@ const monthly_amount = require("../models/monthly_amount.model");
 const last_5_ships = require("../models/last_5_ships.model");
 const next_5_ships = require("../models/next_5_ships.model");
 
+const average = (array) => array.reduce((a, b) => a + b) / array.length;
 
 exports.findAll = async (req, res) => {
   
   try {
-    const avg_amount_response  = await avg_amount.findById(req) 
+    const current_month = new Date().getMonth() + 1;
+    const last_month = current_month - 1;
+
+    const { this_month_family_ranking, last_month_family_ranking, in_transit, orders }  = await avg_amount.findById(req)
+
     const monthly_amount_response  = await monthly_amount.findById(req)
     const last_5_ships_response  = await last_5_ships.findById(req)
     const next_5_ships_response  = await next_5_ships.findById(req)
-  
-    const avg_last_month = avg_amount_response[0]
-    const avg_this_month = avg_amount_response[1]
-    const avg_customers = avg_amount_response[2]
+
     const monthly_data = monthly_amount_response[0]
     const customer_data = monthly_amount_response[1]
+
+    
+    const this_month_orders_average = average(orders.filter(e => e.DataName == "this month avg customer").map(e => e.total_qty))
+    const last_month_orders_average = average(orders.filter(e => e.DataName == "last month avg customer").map(e => e.total_qty))
+
     const response = {
-      "last_month": avg_last_month['total_qty'], 
-      "this_month": avg_this_month['total_qty'], 
-      "customers": avg_customers['total_amount'], 
+      "this_month_family_ranking": this_month_family_ranking, 
+      "last_month_family_ranking": last_month_family_ranking, 
+      "bar_chart_data": {
+        "this_month_orders": orders.find(o => o.DataName == "this month avg customer" && o.month_del_date == current_month), 
+        "last_month_orders": orders.find(o => o.DataName == "last month avg customer" && o.month_del_date == last_month),
+        "this_month_orders_average": this_month_orders_average,
+        "last_month_orders_average": last_month_orders_average,
+      },
+      "in_transit": in_transit,
       "next_5": next_5_ships_response[0],
       "last_5": last_5_ships_response[0],
       "month_data": monthly_data, 
--- app/models/avg_amount.model.js
+++ app/models/avg_amount.model.js
@@ -5,40 +5,40 @@ const Avg_amount = function (avg_amount) {
 };
 
 Avg_amount.findById = async (req) => {
-  //const avg_last_month = avg_amount_response[0]
-  //const avg_this_month = avg_amount_response[1]
-  //const avg_customers = avg_amount_response[2]
+  const options = {
+    method: 'POST',
+    url: 'https://connect.blackfly.dev/execute_sql/',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOnsib3JnYW5pemF0aW9uX2tleSI6InRyYW5zcG9ydF9kX3NxdWFyZWQifSwiaWF0IjoxNjE0Nzk0ODY1LCJzY29wZSI6ImV4ZWN1dGVfc3FsIHJlYWQ6aW1hZ2VzIHdyaXRlOmltYWdlcyJ9.RqH4kpAhNoyi1Tjzg1VZ-kt4CW8xE_nzDeJ5GnmpNk0'
+    }
+  }
+  const this_month_family_ranking_sql = "SELECT ds_billto_id, (SELECT co_name FROM [DBA].companies WHERE co_id = ds_billto_id) AS Customer, YEAR((SELECT top 1 de_arrdate FROM [DBA].disp_events WHERE de_shipment_id = ds_id and de_site = ds_findest_id and de_arrdate IS NOT NULL ORDER BY de_ship_seq ASC)) AS year_del_date, MONTH((SELECT top 1 de_arrdate FROM [DBA].disp_events WHERE de_shipment_id = ds_id and de_site = ds_findest_id and de_arrdate IS NOT NULL ORDER BY de_ship_seq ASC)) AS month_del_date, sum(ds_bill_charge) AS total_qty, ROW_NUMBER() OVER ( ORDER BY total_qty DESC ) row_num FROM [DBA].disp_ship WHERE (year_del_date = YEAR(GETDATE())) AND (month_del_date = MONTH(GETDATE())) AND ds_status IN('K','N','Q','T','W') GROUP BY ds_billto_id, Customer, year_del_date, month_del_date ORDER BY total_qty DESC"
+  const last_month_family_ranking_sql = "SELECT ds_billto_id, (SELECT co_name FROM [DBA].companies WHERE co_id = ds_billto_id) AS Customer, YEAR((SELECT top 1 de_arrdate FROM [DBA].disp_events WHERE de_shipment_id = ds_id and de_site = ds_findest_id and de_arrdate IS NOT NULL ORDER BY de_ship_seq ASC)) AS year_del_date, MONTH((SELECT top 1 de_arrdate FROM [DBA].disp_events WHERE de_shipment_id = ds_id and de_site = ds_findest_id and de_arrdate IS NOT NULL ORDER BY de_ship_seq ASC)) AS month_del_date, sum(ds_bill_charge) AS total_qty, ROW_NUMBER() OVER ( ORDER BY total_qty DESC ) row_num FROM [DBA].disp_ship WHERE (year_del_date = YEAR(GETDATE())) AND (month_del_date = MONTH(GETDATE())-1) AND ds_status IN('K','N','Q','T','W') GROUP BY ds_billto_id, Customer, year_del_date, month_del_date ORDER BY total_qty DESC"
+  const orders_sql = `SELECT 'last month avg customer' AS DataName, YEAR((SELECT top 1 de_arrdate FROM [DBA].disp_events WHERE de_shipment_id = ds_id and de_site = ds_findest_id and de_arrdate IS NOT NULL ORDER BY de_ship_seq ASC)) AS year_del_date, MONTH((SELECT top 1 de_arrdate FROM [DBA].disp_events WHERE de_shipment_id = ds_id and de_site = ds_findest_id and de_arrdate IS NOT NULL ORDER BY de_ship_seq ASC)) AS month_del_date, COUNT(ds_bill_charge) AS total_qty FROM [DBA].disp_ship WHERE (year_del_date = YEAR(GETDATE())) AND (month_del_date BETWEEN 1 AND MONTH(GETDATE())-1) AND ds_status IN('K','N','Q','T','W') ${req.body.id != "Admin" ? `and ds_billto_id = ${req.body.id}` : ""} GROUP BY year_del_date, month_del_date UNION SELECT 'this month avg customer' AS DataName, YEAR((SELECT top 1 de_arrdate FROM [DBA].disp_events WHERE de_shipment_id = ds_id and de_site = ds_findest_id and de_arrdate IS NOT NULL ORDER BY de_ship_seq ASC)) AS year_del_date, MONTH((SELECT top 1 de_arrdate FROM [DBA].disp_events WHERE de_shipment_id = ds_id and de_site = ds_findest_id and de_arrdate IS NOT NULL ORDER BY de_ship_seq ASC)) AS month_del_date, COUNT(ds_bill_charge) AS total_qty FROM [DBA].disp_ship WHERE (year_del_date = YEAR(GETDATE())) AND (month_del_date BETWEEN 1 AND MONTH(GETDATE())) AND ds_status IN('K','N','Q','T','W') ${req.body.id != "Admin" ? `and ds_billto_id = ${req.body.id}` : ""}  GROUP BY year_del_date, month_del_date ORDER BY DataName, month_del_date ASC` 
+  const in_transit_sql = "select count(*) from [DBA].current_shipments where cs_completed < cs_event_count and cs_completed > 0 and (cc_origin_conf = 'T' or cc_findest_conf = 'T')"
 
-  const querys = [                         
-    `SELECT YEAR((SELECT top 1 de_arrdate FROM [DBA].disp_events WHERE de_shipment_id = ds_id and de_site = ds_findest_id and de_arrdate IS NOT NULL ORDER BY de_ship_seq ASC)) AS year_del_date, MONTH((SELECT top 1 de_arrdate FROM [DBA].disp_events WHERE de_shipment_id = ds_id and de_site = ds_findest_id and de_arrdate IS NOT NULL ORDER BY de_ship_seq ASC)) AS month_del_date, COUNT(ds_bill_charge) AS total_qty FROM [DBA].disp_ship WHERE (year_del_date = YEAR(GETDATE())) AND (month_del_date = MONTH(GETDATE())-1) AND ds_status IN('K','N','Q','T','W') ${req.body.id != "Admin" ? `and ds_billto_id = ${req.body.id}` : ""} GROUP BY year_del_date, month_del_date`,
-    `SELECT YEAR((SELECT top 1 de_arrdate FROM [DBA].disp_events WHERE de_shipment_id = ds_id and de_site = ds_findest_id and de_arrdate IS NOT NULL ORDER BY de_ship_seq ASC)) AS year_del_date, MONTH((SELECT top 1 de_arrdate FROM [DBA].disp_events WHERE de_shipment_id = ds_id and de_site = ds_findest_id and de_arrdate IS NOT NULL ORDER BY de_ship_seq ASC)) AS month_del_date, COUNT(ds_bill_charge) AS total_qty FROM [DBA].disp_ship WHERE (year_del_date = YEAR(GETDATE())) AND (month_del_date = MONTH(GETDATE())) AND ds_status IN('K','N','Q','T','W') ${req.body.id != "Admin" ? `and ds_billto_id = ${req.body.id}` : ""} GROUP BY year_del_date, month_del_date`,
-    `SELECT YEAR((SELECT top 1 de_arrdate FROM [DBA].disp_events WHERE de_shipment_id = ds_id and de_site = ds_findest_id and de_arrdate IS NOT NULL ORDER BY de_ship_seq ASC)) AS year_del_date, AVG(ds_bill_charge) AS total_amount FROM [DBA].disp_ship WHERE (year_del_date = YEAR(GETDATE())) AND ds_status IN('K','N','Q','T','W') ${req.body.id != "Admin" ? `and ds_billto_id <> ${req.body.id}` : ""} GROUP BY year_del_date`
-  ]
 
-  let array_results = []
-  for (const query in querys) {
-   
-    const json = querys[query].toString()
-    const options = {
-      method: 'POST',
-      url: 'https://connect.blackfly.dev/execute_sql/',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOnsib3JnYW5pemF0aW9uX2tleSI6InRyYW5zcG9ydF9kX3NxdWFyZWQifSwiaWF0IjoxNjE0Nzk0ODY1LCJzY29wZSI6ImV4ZWN1dGVfc3FsIHJlYWQ6aW1hZ2VzIHdyaXRlOmltYWdlcyJ9.RqH4kpAhNoyi1Tjzg1VZ-kt4CW8xE_nzDeJ5GnmpNk0'
-      },
-      body: JSON.stringify({
-        sql: json
-      }),
-    };
+  const req_this_month_family_ranking = await fetch(options.url, {...options, body: JSON.stringify({sql: this_month_family_ranking_sql})})
+  const data_this_month_family_ranking = await req_this_month_family_ranking.json()
 
-    const req = await fetch(options.url, options)
-    const data = await req.json()
+  const req_last_month_family_ranking = await fetch(options.url, {...options, body: JSON.stringify({sql: last_month_family_ranking_sql})})
+  const data_last_month_family_ranking = await req_last_month_family_ranking.json()
 
-    array_results.push(data.length > 1 ? data : data[0])
-    
+  const req_orders = await fetch(options.url, {...options, body: JSON.stringify({sql: orders_sql})})
+  const data_orders = await req_orders.json()
+
+  const req_in_transit = await fetch(options.url, {...options, body: JSON.stringify({sql: in_transit_sql})})
+  const data_in_transit = await req_in_transit.json()
+
+  const data = {
+    this_month_family_ranking: data_this_month_family_ranking.find(e => e.ds_billto_id == req.body.id),
+    last_month_family_ranking: data_last_month_family_ranking.find(e => e.ds_billto_id == req.body.id),
+    orders: data_orders,
+    in_transit: data_in_transit[0]["count()"],
   }
 
-  return array_results
+  return data
   
 };
 
